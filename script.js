190b8f5ab73b694408d8072e7383b794237fabc3
Sudarshan kakde <60603199+sudarshankakde@users.noreply.github.com>
2021-08-25 05:39:36 -0700

    Add files via upload
    25-08-2021

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 var mode = document.getElementById('mode');
 var mode_icon = document.getElementById('mode_icon');
+var body = document.getElementById('body').style;
+var navbar = document.getElementsByClassName('navbar');
 console.log(mode)
 var value = 0;
-console.log('value=', value)
-// values 0=light , 1=dark
-// apllying if else to see current mode and change mode
-if (value == 0) {
-    mode.addEventListener('click', darkmode);
-    value=1;
-}
-else if(value == 1){
-    mode.addEventListener('click', lightmode);
-    value=0;
-}
-else{
-    alert("something wrong happens");
-}
 
-function darkmode(){
-    
-    var body=document.getElementById('body').style;
-    body.color='white';
-    body.backgroundColor='#181818';
-    mode_icon.className='fa fa-sun-o';
-    value=0;
-    console.log(value)
-}
-function lightmode(){
-    
-    var body=document.getElementById('body').style;
-    body.color='black';
-    body.backgroundColor='white';
-    mode_icon.className='fa fa-moon-o';
-    value=1;
-    console.log(value)
+
+function changemode() {
+    console.log('value=', value)
+    // values 0=light , 1=dark
+    // apllying if else to see current mode and change mode
+    if (value == 0) { 
+        darkmode();
+        value = 1;
+    }
+    else if (value == 1) {
+
+        lightmode();
+        value = 0;
+    }
+    else {
+        alert("something wrong happens");
+    }
+
+    function darkmode() {
+
+        body.color = 'white';
+        body.backgroundColor = '#181818';
+        mode_icon.className = 'fa fa-sun-o';
+        document.getElementById('mode').className="btn btn-light";
+        document.getElementsByClassName('card').className=' card bg-info'
+        console.log(value);
+    }
+    function lightmode() {
+
+        body.color = 'black';
+        body.backgroundColor = 'white';
+        mode_icon.className = 'fa fa-moon-o';
+        document.getElementById('mode').className="btn btn-dark";
+        console.log(value);
+    }
 }
